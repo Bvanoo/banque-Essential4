32d8d80bc7369c81ef476b2d8e58f8af03d19cc9
Bvanoo <bvanoostveldt@gmail.com>
2026-07-17 16:49:19 +0200

    .

--- banque/Epargne.cs
+++ banque/Epargne.cs
@@ -3,13 +3,9 @@
     public class Epargne : Compte
     {
         public Epargne(double Solde) : base(Solde) { }
-
         public Epargne(string numero, Personne titulaire) : base(numero, titulaire) { }
-
         public Epargne(string numero, Personne titulaire, double solde) : base(numero, titulaire, solde) { }
-
         public DateTime DateDernierRetrait { get; private set; }
-
         public override void Retrait(double montant)
         {
             if (montant <= 0)
@@ -23,12 +19,10 @@
                 Console.WriteLine($"Solde insuffisant. Solde actuel : {Solde}");
                 return;
             }
-
             ModifierSolde(Solde - montant);
             DateDernierRetrait = DateTime.Now;
             Console.WriteLine($"Retrait de {montant}€ effectué. Nouveau solde : {Solde}");
         }
-
         public override void Depot(double Montant)
         {
             if (Montant <= 0)
@@ -36,11 +30,9 @@
                 Console.WriteLine("Le montant du dépôt doit être positif.");
                 return;
             }
-
             ModifierSolde(Solde + Montant);
             Console.WriteLine($"Dépôt de {Montant}€ effectué. Nouveau solde : {Solde}");
         }
-
         protected override double CalculInteret()
         {
             return Solde * 0.045;
